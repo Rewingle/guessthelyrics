f3d5fd98c575ddb838eee72fdc0aed5a20f131ed
rewingle <kul.mehmetali@yahoo.com>
2022-02-16 23:41:58 +0300

    e5

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express'); //Line 1
 const app = express(); //Line 2
-const cors = require('cors')
+//const cors = require('cors')
 
 const path = require('path');
 
@@ -9,13 +9,17 @@ const port = process.env.PORT || 5000; //Line 3
 /* if(process.env.NODE_ENV === 'production'){
   app.use(express.static("client/build"))
 }  */
-app.use(cors())
+//app.use(cors())
 
-//app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
- app.get('*', (req, res) => { 
-  res.sendFile(path.join(__dirname + '/client/build/index.html')) 
-}); 
+app.get('*', function(_, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
 // This displays message that the server running and listening to specified port
 app.listen(port, (err) =>{
