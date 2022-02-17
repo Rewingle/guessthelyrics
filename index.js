f253dd7c3aef68e700486679be64b34b8035d170
rewingle <kul.mehmetali@yahoo.com>
2022-02-17 19:11:08 +0300

    m1

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('*', function(_, res) {
   res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
     if (err) {
-      res.status(500).send(err)
+      //res.status(500).send(err)
+      res.send("zaa")
     }
   })
 })
