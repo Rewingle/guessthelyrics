83ed7a90617ef333b52bcdc304a13f2ae9c5b00a
rewingle <kul.mehmetali@yahoo.com>
2022-02-17 19:29:33 +0300

    m5

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ const port = process.env.PORT || 5000; //Line 3
 }  */
 //app.use(cors())
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('*', function(_, res) {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'), function(err) {
     if (err) {
       //res.status(500).send(err)
       res.send("zaa "+port)
